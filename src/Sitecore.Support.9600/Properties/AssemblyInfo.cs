6a0c61dcd398135fe558cbd0cd57c61f2b67806d
Anton Kechashin <atk@sitecore.net>
2017-10-10 12:49:50 +0300

    Added a processor to change the rendering mode for POI items

---
+++ src/Sitecore.Support.9600/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.9600")]
+[assembly: AssemblyProduct("Sitecore.Support.9600")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.9600/XA/Foundation/RenderingVariants/Pipelines/RenderVariantField/FixPoiMarkup.cs
@@ -0,0 +1,17 @@
+﻿namespace Sitecore.Support.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField
+{
+    using Sitecore.XA.Feature.Maps;
+    using Sitecore.XA.Foundation.Variants.Abstractions.Models;
+    using Sitecore.XA.Foundation.Variants.Abstractions.Pipelines.RenderVariantField;
+
+    public class FixPoiMarkup
+    {
+        public void Process(RenderVariantFieldArgs args)
+        {
+            if (args.Item.Parent.TemplateID == Templates.PoiGroup.ID || args.Item.Parent.TemplateID == Templates.PoiGroupingItem.ID) // change RendererMode to Html if the item is a POI
+            {
+                args.RendererMode = RendererMode.Html;
+            }
+        }
+    }
+}
