8474b7f36a0e03869fcdf60e7b72bbc255bd3976
Anton Kechashin <atk@sitecore.net>
2017-10-11 12:15:57 +0300

    Reworked the logic slightly.

--- src/Sitecore.Support.9600/XA/Foundation/RenderingVariants/Pipelines/RenderVariantField/FixPoiMarkup.cs
+++ src/Sitecore.Support.9600/XA/Foundation/RenderingVariants/Pipelines/RenderVariantField/FixPoiMarkup.cs
@@ -1,6 +1,7 @@
 ﻿namespace Sitecore.Support.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField
 {
-    using Sitecore.XA.Feature.Maps;
+    using Sitecore.XA.Foundation.Geospatial;
+    using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
     using Sitecore.XA.Foundation.Variants.Abstractions.Models;
     using Sitecore.XA.Foundation.Variants.Abstractions.Pipelines.RenderVariantField;
 
@@ -8,7 +9,7 @@
     {
         public void Process(RenderVariantFieldArgs args)
         {
-            if (args.Item.Parent.TemplateID == Templates.PoiGroup.ID || args.Item.Parent.TemplateID == Templates.PoiGroupingItem.ID) // change RendererMode to Html if the item is a POI
+            if (args.Item.DoesItemInheritFrom(Templates.IPoi.ID)) // change RendererMode to Html if the item is a POI
             {
                 args.RendererMode = RendererMode.Html;
             }
